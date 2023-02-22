b3d823198f4fd42b14dec132ce768683ad34a3bb
Jannick Sørensen <jann6665@zbc.dk>
2023-02-22 12:54:37 +0100

    Changes

--- Sessions/Controllers/DeleteController.cs
+++ Sessions/Controllers/DeleteController.cs
@@ -10,15 +10,19 @@ namespace Sessions.Controllers
         [HttpDelete]
         public string Delete(string name)
         {
+            bool wasRemoved = false;
             if (HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart") != null)
             {
-                bool wasRemoved = false;
+                
                 Product productToRemove = new Product();
                 List<Product> products = (List<Product>)HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart")!;
                 foreach (Product product in products)
                 {
-                    if(product.Name == name.ToLower()) productToRemove = product;
-                    wasRemoved = true;
+                    if (product.Name == name.ToLower())
+                    {
+                        productToRemove = product;
+                        wasRemoved = true;
+                    }
                 }
                 if (wasRemoved)
                 {
