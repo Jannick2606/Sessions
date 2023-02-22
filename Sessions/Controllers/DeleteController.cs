e0e5e62c1b17e2291ee93dedd6ed82a4b45ba81a
Jannick Sørensen <jann6665@zbc.dk>
2023-02-22 12:34:43 +0100

    Add project files.

---
+++ Sessions/Controllers/DeleteController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sessions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteController : ControllerBase
+    {
+        [HttpDelete]
+        public string Delete(string name)
+        {
+            if (HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart") != null)
+            {
+                Product removedProduct = new Product();
+                List<Product> products = (List<Product>)HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart")!;
+                foreach (Product product in products)
+                {
+                    if(product.Name == name.ToLower()) removedProduct = product;
+                }
+                products.Remove(removedProduct);
+                HttpContext.Session.SetObjectAsJson("ShoppingCart", products);
+                return $"{removedProduct.Name} was removed";
+            }
+            return "Session is empty";
+        }
+
+
+        [HttpDelete]
+        [Route("[Action]")]
+        public void DeleteAllItems()
+        {
+            if (HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart") != null)
+            {
+                HttpContext.Session.Clear();
+            }
+        }
+    }
+}
---
+++ Sessions/Controllers/ShoppingcartController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sessions.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingcartController : ControllerBase
+    {
+
+        [HttpGet]
+        public Product Get(string name, int price)
+        {
+            List<Product> products = new List<Product>();
+            Product product = new Product() { Name = name.ToLower(), Price = price };
+            products.Add(product);
+            if (HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart") != null)
+            {
+                products.AddRange(HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart")!);
+            }
+            HttpContext.Session.SetObjectAsJson("ShoppingCart", products);
+            return product;
+        }
+        [HttpGet]
+        [Route("[Action]")]
+        public IEnumerable<Product> GetFromSession()
+        {
+            if (HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart") != null)
+            {
+                return HttpContext.Session.GetObjectFromJson<IEnumerable<Product>>("ShoppingCart")!;
+            }
+            else return Enumerable.Empty<Product>();
+        }
+    }
+}
